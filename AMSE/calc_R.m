a7a6406a57ac44ef9a7057c8efcdce558e7cff67
Ori Kenig <orikenig@gmail.com>
2023-11-28 01:14:14 +0200

    Edited Calc_R.m
    Removed unnecessary condition fixing in calc_R

--- AMSE/calc_R.m
+++ AMSE/calc_R.m
@@ -8,16 +8,6 @@ function  [R,C,D] = calc_R(all_x,h,GMMStruct)
     
     [D,v] = calc_D(all_x,h,GMMStruct);
     
-    %{
-    cond_C = cond(C);
-    if(cond_C > 30)
-        %warning("S with large condition number")
-        C = C + 0.01 * 10^floor(log10(trace(C))) * log10(cond_C) * eye(GMMStruct.len);
-    end
-
-    C = (C+C')./2;
-    %}
-    
     tmp = ( (C*N) \ v );
 
     R = (tmp) * (tmp');
